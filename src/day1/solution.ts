992811d6acf2b13551dcc7e55233b19e7461d6a1
Ondřej Mastík <ondramastik@outlook.com>
2022-12-07 21:45:32 +0100

    Added solution do day 1 part 2

--- src/day1/solution.ts
+++ src/day1/solution.ts
@@ -1,23 +1,31 @@
 import {readFile} from 'fs';
 
-function solve(data: string) {
-    const elves = data.split(/\r?\n\r?\n/).map(value => value.split(/\r?\n/));
-
-    const elvesFood = elves.map(counts => counts.map((count) => {
-        return Number(count);
-    }));
+/**
+ * Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
+ */
+function solvePart1(data: string): number {
+    const elves = parseTestData(data)
 
     let maxFood = 0;
-    elvesFood.forEach((foodCounts) => {
-        const totalFood = foodCounts.reduce((accumulator, value) => {
-            return accumulator + value;
-        }, 0);
+    elves.forEach((foodCounts) => {
+        const totalFood = sumArray(foodCounts)
         if (totalFood > maxFood) {
             maxFood = totalFood;
         }
     })
 
-    console.log(maxFood)
+    return maxFood;
+}
+
+/**
+ * Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?
+ */
+function solvePart2(data: string): number {
+    const elves = parseTestData(data)
+
+    const foodCountPerElf = elves.map((foodCounts) => sumArray(foodCounts))
+
+    return sumArray(foodCountPerElf.sort().slice(-3))
 }
 
 function loadTestData(): Promise<string> {
@@ -29,4 +37,22 @@ function loadTestData(): Promise<string> {
     })
 }
 
-loadTestData().then((data) => solve(data))
+function parseTestData(data: string): number[][] {
+    const elves = data.split(/\r?\n\r?\n/).map(value => value.split(/\r?\n/));
+
+    return elves.map(counts => counts.map((count) => {
+        return Number(count);
+    }));
+}
+
+function sumArray(numbers: number[]): number {
+    return numbers.reduce((accumulator, value) => {
+        return accumulator + value;
+    }, 0);
+}
+
+loadTestData()
+    .then((data) => {
+        console.log(`Answer to part 1 is ${solvePart1(data)}`)
+        console.log(`Answer to part 2 is ${solvePart2(data)}`)
+    })
