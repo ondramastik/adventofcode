02e71a827f687617a27c9bbed38799c8bd52a913
Ondřej Mastík <ondramastik@outlook.com>
2022-12-08 10:39:22 +0100

    Added solution to day 2 part 2

--- src/day2/solution.ts
+++ src/day2/solution.ts
@@ -4,6 +4,23 @@ import {loadTestData} from "../utils";
  * What would your total score be if everything goes exactly according to your strategy guide?
  */
 function solvePart1(options: [OpponentOption, PlayerOption][]): number {
+    /**
+     * @return total score gainł
+     */
+    const evaluateRound: (option1: OpponentOption, option2: PlayerOption) => number = (option1, option2) => {
+        if (option1 === OpponentOption.Rock && option2 === PlayerOption.Rock
+            || option1 === OpponentOption.Paper && option2 === PlayerOption.Paper
+            || option1 === OpponentOption.Scissors && option2 === PlayerOption.Scissors) {
+            return getResultPoints(RoundResult.Draw) + getOptionPoints(option2)
+        } else if (option1 == OpponentOption.Rock && option2 == PlayerOption.Scissors
+            || option1 == OpponentOption.Paper && option2 == PlayerOption.Rock
+            || option1 == OpponentOption.Scissors && option2 == PlayerOption.Paper) {
+            return getOptionPoints(option2)
+        } else {
+            return getResultPoints(RoundResult.Win) + getOptionPoints(option2)
+        }
+    }
+
     return options.map(([option1, option2]) => evaluateRound(option1, option2))
         .reduce((accumulator, value) => {
             return accumulator + value;
@@ -11,23 +28,66 @@ function solvePart1(options: [OpponentOption, PlayerOption][]): number {
 }
 
 /**
- * @return total score gain
+ * What would your total score be if everything goes exactly according to your strategy guide?
  */
-function evaluateRound(option1: OpponentOption, option2: PlayerOption): number {
-    if (option1 === OpponentOption.Rock && option2 === PlayerOption.Rock
-        || option1 === OpponentOption.Paper && option2 === PlayerOption.Paper
-        || option1 === OpponentOption.Scissors && option2 === PlayerOption.Scissors) {
-        return 3 + getOptionScore(option2)
-    } else if (option1 == OpponentOption.Rock && option2 == PlayerOption.Scissors
-        || option1 == OpponentOption.Paper && option2 == PlayerOption.Rock
-        || option1 == OpponentOption.Scissors && option2 == PlayerOption.Paper) {
-        return 0 + getOptionScore(option2)
-    } else {
-        return 6 + getOptionScore(option2)
+function solvePart2(options: [OpponentOption, RoundResult][]): number {
+    /**
+     * @return total score gain
+     */
+    const evaluateRound: (option1: OpponentOption, result: RoundResult) => number = (option1, result) => {
+        let optionPoints = 0;
+        switch (result) {
+            case RoundResult.Lose:
+                switch (option1) {
+                    case OpponentOption.Rock:
+                        optionPoints = getOptionPoints(PlayerOption.Scissors)
+                        break
+                    case OpponentOption.Paper:
+                        optionPoints = getOptionPoints(PlayerOption.Rock)
+                        break
+                    case OpponentOption.Scissors:
+                        optionPoints = getOptionPoints(PlayerOption.Paper)
+                        break
+                }
+                break
+            case RoundResult.Draw:
+                switch (option1) {
+                    case OpponentOption.Rock:
+                        optionPoints = getOptionPoints(PlayerOption.Rock)
+                        break
+                    case OpponentOption.Paper:
+                        optionPoints = getOptionPoints(PlayerOption.Paper)
+                        break
+                    case OpponentOption.Scissors:
+                        optionPoints = getOptionPoints(PlayerOption.Scissors)
+                        break
+                }
+                break
+            case RoundResult.Win:
+                switch (option1) {
+                    case OpponentOption.Rock:
+                        optionPoints = getOptionPoints(PlayerOption.Paper)
+                        break
+                    case OpponentOption.Paper:
+                        optionPoints = getOptionPoints(PlayerOption.Scissors)
+                        break
+                    case OpponentOption.Scissors:
+                        optionPoints = getOptionPoints(PlayerOption.Rock)
+                        break
+                }
+                break
+        }
+
+        return getResultPoints(result) + optionPoints;
     }
+
+    return options.map(([option1, option2]) => evaluateRound(option1, option2))
+        .reduce((accumulator, value) => {
+            return accumulator + value;
+        }, 0);
 }
 
-function getOptionScore(option: PlayerOption) {
+function getOptionPoints(option: PlayerOption) {
     switch (option) {
         case PlayerOption.Rock:
             return 1;
@@ -38,15 +98,26 @@ function getOptionScore(option: PlayerOption) {
     }
 }
 
-function parseTestData(rawData: string): [OpponentOption, PlayerOption][] {
+function getResultPoints(result: RoundResult) {
+    switch (result) {
+        case RoundResult.Lose:
+            return 0;
+        case RoundResult.Draw:
+            return 3;
+        case RoundResult.Win:
+            return 6;
+    }
+}
+
+function parseTestData<T>(rawData: string): [OpponentOption, T][] {
     return rawData.split(/\r?\n/)
-        .map(value => [value.charAt(0) as OpponentOption, value.charAt(2) as PlayerOption]);
+        .map(value => [value.charAt(0) as OpponentOption, value.charAt(2) as unknown as T]);
 }
 
 loadTestData("src/day2/input")
-    .then((data) => parseTestData(data))
     .then((data) => {
-        console.log(`Answer to part 1 is ${solvePart1(data)}`)
+        console.log(`Answer to part 1 is ${solvePart1(parseTestData(data))}`)
+        console.log(`Answer to part 2 is ${solvePart2(parseTestData(data))}`)
     })
 
 enum OpponentOption {
@@ -59,4 +130,10 @@ enum PlayerOption {
     Rock = 'X',
     Paper = 'Y',
     Scissors = 'Z',
+}
+
+enum RoundResult {
+    Lose = 'X',
+    Draw = 'Y',
+    Win = 'Z',
 }
